35558c4735c888c91ac819d613131944be1a7d53
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-06 20:05:53 -0600

    Add to controller to show error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,23 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      user.user_detail = UserDetail.new(user_detail_params)
-      redirect_to '/profile'
-      flash[:success] = "Registration sucessful! You are now logged in."
-    else
-      flash[:error] = "Registration unsucessful. Try again! 😱"
+      detail = UserDetail.new(user_detail_params)
+      detail.user_id = user.id
+      if detail.valid?
+        user.user_detail = detail
+        flash[:success] = "Registration sucessful! You are now logged in."
+        redirect_to '/profile'
+      elsif detail.errors.messages.values.empty?
+        flash[:error] = "Email is already in use."
+        redirect_to '/register'
+      else
+        flash[:error] = "Registration unsucessful. Please fill in required fields. 😱"
+        redirect_to '/register'
+      end
     end
   end
 
   def show
-
   end
 
   private
