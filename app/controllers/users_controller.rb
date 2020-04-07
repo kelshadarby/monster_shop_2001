f900c8d37ceb5434bfcd5656a9130d7837a8da08
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 16:28:08 -0600

    Update create action to save user in session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Registration sucessful! You are now logged in."
       redirect_to profile_path
     else
