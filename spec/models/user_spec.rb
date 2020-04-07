dde8b3a462b103a91cc6fe387b0ca95a1db28bd0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 16:25:39 -0600

    Add validation for role attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email_address }
     it { expect(subject).to validate_uniqueness_of(:email_address).with_message("Email is already in use.") }
     it { should validate_presence_of :password }
+    it { should validate_presence_of :role}
   end
   describe "relationships" do
     it { should have_one :user_detail }
