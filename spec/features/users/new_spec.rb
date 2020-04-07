f6ba2f4641428c56c9a94b0fc99c05cdfe2a05f0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 14:45:56 -0600

    Remove current path expects for render

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,78 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe "As a user", type: :feature do
-  it "can register a new user with a unique email" do
-    visit '/'
+  describe 'When creating an new user account I' do
+    before(:all) do
 
-    click_link 'Register'
+      @user1 = User.new(
+        email_address: "user1@example.com",
+        password: "password",
+        user_detail: UserDetail.new(        
+          name: "User 1",
+          street_address: "123 Example St",
+          city: "Userville",
+          state: "State 1",
+          zip_code: "12345"
+        )
+      )
+      @user2 = User.new(
+        email_address: "user2@example.com",
+        password: "password",
+        user_detail: UserDetail.new(
+          name: "User 2",
+          street_address: "123 Example St",
+          city: "Userville",
+          state: "State 2",
+          zip_code: "12345"
+        )
+      )
+    end
+    it "can register a new user with a unique email" do
+      visit '/'
 
-    expect(current_path).to eq '/register'
+      click_link 'Register'
 
-    fill_in "Name", with: "User 1"
-    fill_in "Street Address", with: "123 Example St"
-    fill_in "City", with: "Userville"
-    fill_in "State", with: "State 1"
-    fill_in "Zip Code", with: "12345"
-    fill_in "Email Address", with: "user@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Confirm Password", with: "password"
-    click_button "Submit"
+      expect(current_path).to eq '/register'
+      fill_in "user_detail_attributes[name]", with: @user1.user_detail.name
+      fill_in "user_detail_attributes[street_address]", with: @user1.user_detail.street_address
+      fill_in "user_detail_attributes[city]", with: @user1.user_detail.city
+      fill_in "user_detail_attributes[state]", with: @user1.user_detail.state
+      fill_in "user_detail_attributes[zip_code]", with: @user1.user_detail.zip_code
+      fill_in "Email Address", with: @user1.email_address
+      fill_in "Password", with: @user1.password
+      fill_in "Confirm Password", with: @user1.password
+      click_button "Submit"
+      
+      expect(current_path).to eq '/profile'
+      expect(page).to have_content("Registration sucessful! You are now logged in.")
+    end
+    it "cannot register a user if form is incomplete" do
+      visit '/register'
 
-    expect(current_path).to eq '/profile'
-    expect(page).to have_content("Registration sucessful! You are now logged in.")
-  end
-  it "cannot register a user if form is incomplete" do
+      fill_in "user_detail_attributes[name]", with: @user1.user_detail.name
+      # fill_in "user_detail_attributes[street_address]", with: @user1.user_detail.street_address
+      fill_in "user_detail_attributes[city]", with: @user1.user_detail.city
+      fill_in "user_detail_attributes[state]", with: @user1.user_detail.state
+      fill_in "user_detail_attributes[zip_code]", with: @user1.user_detail.zip_code
+      fill_in "Email Address", with: @user1.email_address
+      fill_in "Password", with: @user1.password
+      fill_in "Confirm Password", with: @user1.password
+      click_button "Submit"
 
-    visit '/register'
-    fill_in "Name", with: "User 1"
-    # fill_in "Street Address", with: "123 Example St"
-    fill_in "City", with: "Userville"
-    fill_in "State", with: "State 1"
-    fill_in "Zip Code", with: "12345"
-    fill_in "Email Address", with: "user@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Confirm Password", with: "password"
-    click_button "Submit"
+      expect(page).to have_content("User detail street address can't be blank")
 
-    expect(page).to have_content("Registration unsucessful. Please fill in required fields. 😱")
-    expect(current_path).to eq '/register'
-  end
-  it "cannot create a user with a duplicate email address" do
-    visit '/register'
+    end
+    it "cannot create a user with a duplicate email address" do
 
-    fill_in "Name", with: "User 1"
-    fill_in "Street Address", with: "123 Example St"
-    fill_in "City", with: "Userville"
-    fill_in "State", with: "State 1"
-    fill_in "Zip Code", with: "12345"
-    fill_in "Email Address", with: "user@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Confirm Password", with: "password"
-    click_button "Submit"
+      visit '/register'
 
-    visit '/register'
+      fill_in "user_detail_attributes[name]", with: @user1.user_detail.name
+      fill_in "user_detail_attributes[street_address]", with: @user1.user_detail.street_address
+      fill_in "user_detail_attributes[city]", with: @user1.user_detail.city
+      fill_in "user_detail_attributes[state]",  with: @user1.user_detail.state
+      fill_in "user_detail_attributes[zip_code]", with: @user1.user_detail.zip_code
+      fill_in "Email Address", with: @user1.email_address
+      fill_in "Password", with: @user1.password
+      fill_in "Confirm Password", with: @user1.password
+      click_button "Submit"
 
-    fill_in "Name", with: "User 2"
-    fill_in "Street Address", with: "124 Example St"
-    fill_in "City", with: "Usertown"
-    fill_in "State", with: "State 2"
-    fill_in "Zip Code", with: "12346"
-    fill_in "Email Address", with: "user@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Confirm Password", with: "password"
-    click_button "Submit"
+      visit '/register'
 
-    expect(current_path).to eq '/register'
-    expect(User.all.count).to eq(1)
+      @user2.email_address = @user1.email_address
+      
+      fill_in "user_detail_attributes[name]", with: @user2.user_detail.name
+      fill_in "user_detail_attributes[street_address]", with: @user2.user_detail.street_address
+      fill_in "user_detail_attributes[city]", with: @user2.user_detail.city
+      fill_in "user_detail_attributes[state]", with: @user2.user_detail.state
+      fill_in "user_detail_attributes[zip_code]", with: @user2.user_detail.zip_code
+      fill_in "Email Address", with: @user2.email_address
+      fill_in "Password", with: @user2.password
+      fill_in "Confirm Password", with: @user2.password
+      click_button "Submit"
 
-    expect(find_field("Name").value).to eq("User 2")
-    expect(find_field("Street Address").value).to eq("124 Example St")
-    expect(find_field("City").value).to eq("Usertown")
-    expect(find_field("State").value).to eq("State 2")
-    expect(find_field("Zip Code").value).to eq("12346")
-    expect(find_field("Email Address").value).to eq("")
-    expect(find_field("Password").value).to eq("")
+      expect(User.all.count).to eq(1)
 
-    expect(page).to have_content("Email is already in use.")
+      expect(find_field("user_detail_attributes[name]").value).to eq(@user2.user_detail.name)
+      expect(find_field("user_detail_attributes[street_address]").value).to eq(@user2.user_detail.street_address)
+      expect(find_field("user_detail_attributes[city]").value).to eq(@user2.user_detail.city)
+      expect(find_field("user_detail_attributes[state]").value).to eq(@user2.user_detail.state)
+      expect(find_field("user_detail_attributes[zip_code]").value).to eq(@user2.user_detail.zip_code)
+      expect(find_field("Email Address").value).to eq("")
+      expect(find_field("Password").value).to eq("")
+
+      expect(page).to have_content("Email is already in use.")
+    end
+
   end
 end
