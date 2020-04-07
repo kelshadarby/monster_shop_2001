5932debc9143814c423afc300a21516c5fea88db
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-06 18:29:35 -0600

    Add password verification to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  attr_accessor :password_confirmation
+
   validates_presence_of :email_address
   validates :email_address, uniqueness: true
-  validates_presence_of :password_digest
+  validates_presence_of :password, require: true
+  validates_confirmation_of :password
 
   has_secure_password
 
