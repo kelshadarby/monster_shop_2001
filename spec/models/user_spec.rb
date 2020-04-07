44d7c6e2b2eba9d55b48a891a869d79c78d231c5
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-06 18:29:48 -0600

    Change password verification in user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :email_address }
     it { should validate_uniqueness_of :email_address }
-    it { should validate_presence_of :password_digest }
+    it { should validate_presence_of :password }
   end
   describe "relationships" do
     it { should have_one :user_detail }
