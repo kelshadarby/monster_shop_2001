de0e8e51445d3e8f11b26dabfa518e8ebb050e97
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 14:43:48 -0600

    Add associated validation to userdetail model

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,8 +1,5 @@
 class UserDetail < ApplicationRecord
-  validates_presence_of :name,
-                        :street_address,
-                        :city,
-                        :state,
-                        :zip_code
+  validates_presence_of :name, :street_address, :city, :state, :zip_code
+  validates_associated :user
   belongs_to :user
 end
