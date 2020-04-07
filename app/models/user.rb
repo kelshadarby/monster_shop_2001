f2ca80a9a496e659045283161cd479337a68d65e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 14:44:48 -0600

    Add custom error message to email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   attr_accessor :password_confirmation
 
   validates_presence_of :email_address
-  validates :email_address, uniqueness: true
+  validates :email_address, uniqueness: {message: "Email is already in use."}
   validates_presence_of :password, require: true
   validates_confirmation_of :password
-
+  
+  
   has_secure_password
-
+  
   has_one :user_detail
+  accepts_nested_attributes_for :user_detail
 end
