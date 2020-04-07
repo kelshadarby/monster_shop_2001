4ec20ba91988e0ce05155755f5bbbbc70f19a071
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-07 15:10:28 -0600

    Update User model test with custom validation message

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :email_address }
-    it { should validate_uniqueness_of :email_address }
+    it { expect(subject).to validate_uniqueness_of(:email_address).with_message("Email is already in use.") }
     it { should validate_presence_of :password }
   end
   describe "relationships" do
