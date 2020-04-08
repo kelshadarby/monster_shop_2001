516171480112f0a73ff0c5f620eaaf1876a64c8a
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-07 21:35:38 -0600

    Add merchant nav bar test to spec

--- spec/features/users/nav_bar_spec.rb
+++ spec/features/users/nav_bar_spec.rb
@@ -1,30 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "As a user (role 1)", type: :feature do
-  it "can see profile page and logout links without login or register links" do
-    user = User.create(
-      email_address: "user1@example.com",
-      password: "password",
-      user_detail: UserDetail.new(
-        name: "User 1",
-        street_address: "123 Example St",
-        city: "Userville",
-        state: "State 1",
-        zip_code: "12345"
+  describe "default (role 1)" do
+    it "can see profile page and logout links without login or register links" do
+      default_user = User.create(
+        email_address: "user1@example.com",
+        password: "password",
+        role: "default",
+        user_detail: UserDetail.new(
+          name: "User 1",
+          street_address: "123 Example St",
+          city: "Userville",
+          state: "State 1",
+          zip_code: "12345"
+        )
       )
-    )
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
 
-    visit "/default/profile"
+      visit "/default/profile"
 
-    expect(page).to have_link("Home")
-    expect(page).to have_link("All Merchants")
-    expect(page).to have_link("All Items")
-    expect(page).to have_link("Cart: 0")
-    expect(page).to have_link("View My Profile")
-    expect(page).to have_link("Logout")
+      expect(page).to have_link("Home")
+      expect(page).to have_link("All Merchants")
+      expect(page).to have_link("All Items")
+      expect(page).to have_link("Cart: 0")
+      expect(page).to have_link("View My Profile")
+      expect(page).to have_link("Logout")
 
-    expect(page).to have_content("Logged in as #{user.user_detail.name}")
+      expect(page).to have_content("Logged in as #{default_user.user_detail.name}")
+    end
+  end
+  describe "as a merchant (role 2)" do
+    it "can see same links as regular user plus link to merchant dashboard" do
+      merchant_user = User.create(
+        email_address: "user1@example.com",
+        password: "password",
+        role: "merchant",
+        user_detail: UserDetail.new(
+          name: "User 1",
+          street_address: "123 Example St",
+          city: "Userville",
+          state: "State 1",
+          zip_code: "12345"
+        )
+      )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_user)
+
+      visit "/"
+
+      expect(page).to have_link("Home")
+      expect(page).to have_link("All Merchants")
+      expect(page).to have_link("All Items")
+      expect(page).to have_link("Cart: 0")
+      expect(page).to have_link("View My Profile")
+      expect(page).to have_link("Logout")
+      expect(page).to have_link("Dashboard")
+    end
   end
 end
