0841d2180806fb04a0e107de559a907732f553c2
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-08 17:38:16 -0600

    Add base merchant controller

---
+++ app/controllers/merchant/base_controller.rb
@@ -0,0 +1,10 @@
+class Merchant::BaseController < ApplicationController
+  before_action :require_merchant
+  
+  
+  private
+
+  def require_merchant
+    render file: 'public/404', status: 404 unless current_merchant?
+  end
+end
