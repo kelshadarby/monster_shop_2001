468a13279997ce58ffa9f25de2d5e35176e3f4c2
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-07 20:13:30 -0600

    Create user story 3, nav bar view for a role 1 user test

---
+++ spec/features/users/nav_bar_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "As a user (role 1)", type: :feature do
+  it "can see profile page and logout links without login or register links" do
+    user = User.create(
+      email_address: "user1@example.com",
+      password: "password",
+      user_detail: UserDetail.new(
+        name: "User 1",
+        street_address: "123 Example St",
+        city: "Userville",
+        state: "State 1",
+        zip_code: "12345"
+      )
+    )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/user/profile"
+
+    expect(page).to have_link("Home")
+    expect(page).to have_link("All Merchants")
+    expect(page).to have_link("All Items")
+    expect(page).to have_link("Cart: 0")
+    expect(page).to have_link("View My Profile")
+    expect(page).to have_link("Logout")
+
+    expect(page).to have_content("Logged in as #{user.user_detail.name}")
+  end
+end
