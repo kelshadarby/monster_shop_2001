bea0d0a101cd006d6f59f333bfffa53c407067d8
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-08 14:57:47 -0600

    Edit user creation flash message name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Registration sucessful! You are now logged in."
+      flash[:notice] = "Registration sucessful! You are now logged in."
       redirect_to profile_path
     else
       flash[:error] = @user.errors.full_messages.to_sentence
