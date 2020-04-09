86c41a096a7ef3a62516cdbd0377e00849924be1
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-09 17:54:16 -0600

    Add test for email already in use

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -1,8 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "As a signed in user, when I visit /profile", type: :feature do
-    it "there is a link I can click to edit profile information" do
-      user1 = User.new(
+  it "there is a link I can click to edit profile information" do
+    user1 = User.new(
+      email_address: "user1@example.com",
+      password: "password",
+      name: "User 1",
+      street_address: "123 Example St",
+      city: "Userville",
+      state: "State 1",
+      zip_code: "12345"
+    )
+    user1.save
+    visit login_path
+    fill_in "Email Address", with: user1.email_address
+    fill_in "Password", with: user1.password
+    click_button "Login"
+
+    visit "/profile"
+    expect(page).to have_link("Edit Profile")
+    click_link("Edit Profile")
+
+    fill_in "Name", with: "George"
+    fill_in "Street Address", with: "123 fake street"
+    fill_in "City", with: "Denver"
+    fill_in "State", with: "CO"
+    fill_in "Zip Code", with: "80205"
+
+    click_button "Update Info"
+
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Your information has been updated.")
+    expect(page).to have_content("George")
+    expect(page).to have_content("123 fake street")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("CO")
+    expect(page).to have_content("80205")
+    expect(page).to_not have_content("123 Example St")
+    expect(page).to_not have_content("User 1")
+  end
+
+  describe 'Attempting to set my email to an email already in the database' do
+    it 'I am shown the profile edit page with a flash message indicating the email is in use' do
+      user1 = User.create!(
         email_address: "user1@example.com",
         password: "password",
         name: "User 1",
@@ -11,32 +51,26 @@ RSpec.describe "As a signed in user, when I visit /profile", type: :feature do
         state: "State 1",
         zip_code: "12345"
       )
-      user1.save
-      visit '/login'
-      fill_in "Email Address", with: user1.email_address
-      fill_in "Password", with: user1.password
+      user2 = User.create!(
+        email_address: "user2@example.com",
+        password: "password",
+        name: "User 2",
+        street_address: "123 Example St",
+        city: "User2ville",
+        state: "State 2",
+        zip_code: "12345"
+      )
+      visit login_path
+      fill_in "Email Address", with: user2.email_address
+      fill_in "Password", with: user2.password
       click_button "Login"
 
-      visit "/profile"
-      expect(page).to have_link("Edit Profile")
-      click_link("Edit Profile")
-
-      fill_in "Name", with: "George"
-      fill_in "Street Address", with: "123 fake street"
-      fill_in "City", with: "Denver"
-      fill_in "State", with: "CO"
-      fill_in "Zip Code", with: "80205"
-
+      visit profile_edit_path
+      fill_in "Email Address", with: user1.email_address
       click_button "Update Info"
 
-      expect(current_path).to eq("/profile")
-      expect(page).to have_content("Your information has been updated.")
-      expect(page).to have_content("George")
-      expect(page).to have_content("123 fake street")
-      expect(page).to have_content("Denver")
-      expect(page).to have_content("CO")
-      expect(page).to have_content("80205")
-      expect(page).to_not have_content("123 Example St")
-      expect(page).to_not have_content("User 1")
+      expect(page).to have_content("Email is already in use.")
+      expect(page).to have_button("Update Info")
     end
+  end
 end
