b74c0c2a71ae02946ed3b224b44e2955e28f013a
Fred Rondina <fredrondina96@gmail.com>
2020-04-09 18:24:22 -0600

    added passing test for us 21

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -39,4 +39,40 @@ RSpec.describe "As a signed in user, when I visit /profile", type: :feature do
       expect(page).to_not have_content("123 Example St")
       expect(page).to_not have_content("User 1")
     end
+
+    it "there is a link to change user password" do
+      user1 = User.new(
+        email_address: "user1@example.com",
+        password: "password",
+        name: "User 1",
+        street_address: "123 Example St",
+        city: "Userville",
+        state: "State 1",
+        zip_code: "12345"
+      )
+      user1.save
+      visit '/login'
+      fill_in "Email Address", with: user1.email_address
+      fill_in "Password", with: user1.password
+      click_button "Login"
+
+      visit "/profile"
+      expect(page).to have_link("Change Password")
+      click_link("Change Password")
+
+      fill_in "Password", with: "BeanBurrito"
+      fill_in "Confirm Password", with: "BeanBurritoo"
+
+      click_button "Update Password"
+
+      expect(page).to have_content("Passwords Must Match")
+
+      fill_in "Password", with: "BeanBurrito"
+      fill_in "Confirm Password", with: "BeanBurrito"
+
+      click_button "Update Password"
+
+      expect(page).to have_content("Password has been changed.")
+      expect(current_path).to eq("/profile")
+    end
 end
