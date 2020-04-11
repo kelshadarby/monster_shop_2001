d0063fdd4c535e9895c3b4794b5bafc38a2f03e8
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-11 16:20:54 -0600

    Add remove_item test

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Cart, type: :model do
   describe "instance methods" do
+
     it "add_item" do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -20,18 +21,20 @@ RSpec.describe Cart, type: :model do
       expect(cart.contents[paper.id.to_s]).to eq(1)
       expect(cart.contents[pencil.id.to_s]).to eq(1)
     end
+
     it "subtract_item" do
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
       cart = Cart.new(tire.id.to_s => 1)
-      
+
       cart.add_item(tire.id.to_s)
       expect(cart.contents[tire.id.to_s]).to eq(2)
 
       cart.subtract_item(tire.id.to_s)
       expect(cart.contents[tire.id.to_s]).to eq(1)
     end
+
     it "total_items" do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -48,6 +51,7 @@ RSpec.describe Cart, type: :model do
 
       expect(cart.total_items).to eq(4)
     end
+
     it "items" do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -69,6 +73,7 @@ RSpec.describe Cart, type: :model do
 
       expect(cart.items).to eq(expected)
     end
+
     it "subtotal" do
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
@@ -79,6 +84,7 @@ RSpec.describe Cart, type: :model do
 
       expect(cart.subtotal(tire)).to eq(200)
     end
+
     it "total" do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -95,5 +101,16 @@ RSpec.describe Cart, type: :model do
 
       expect(cart.total).to eq(222)
     end
+
+    it "remove_item" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+      cart = Cart.new(tire.id.to_s => 1)
+      cart.remove_item(tire.id.to_s)
+      expect(cart.total).to eq(0)
+      expect(cart.contents).to eq({})
+    end
+    
   end
 end
