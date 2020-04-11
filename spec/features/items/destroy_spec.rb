6d4af3e6934afc877a24d867b0b5aedb5abded8b
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-11 10:16:52 -0600

    Add formatting

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe 'item delete', type: :feature do
         zip_code: '12345'
       )
     end
+    
     it 'I can delete an item' do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
