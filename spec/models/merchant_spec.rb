edd737869d2043dc410652f37525e79a4fb7a50e
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-12 22:15:31 -0600

    Add model method test for created methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,6 +15,7 @@ describe Merchant, type: :model do
   end
 
   describe 'instance methods' do
+
     before(:each) do
       @user = User.create!(
         email_address: 'user1@example.com',
@@ -29,6 +30,7 @@ describe Merchant, type: :model do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     end
+
     it 'no_orders' do
       expect(@meg.no_orders?).to eq(true)
 
@@ -72,6 +74,16 @@ describe Merchant, type: :model do
       order_3.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
 
       expect(@meg.pending_orders).to include(order_1, order_2, order_3)
+    end
+
+    it "disabled?" do
+      expect(@meg.disabled?).to eq(false)
+    end
+
+    it "disable" do
+      expect(@meg.disabled?).to eq(false)
+      @meg.disable
+      expect(@meg.disabled?).to eq(true)
     end
   end
 end
