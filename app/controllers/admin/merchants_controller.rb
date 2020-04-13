c81bba20f7f4575f4b90d5b86ebab5dea2d15d50
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-12 22:12:18 -0600

    Add disable method to merchants controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,11 @@ class Admin::MerchantsController < Admin::BaseController
   def index
     @merchants = Merchant.all
   end
+
+  def disable
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.disable
+    flash[:success] = "#{merchant.name} has been disabled"
+    redirect_to "/admin/merchants"
+  end
 end
