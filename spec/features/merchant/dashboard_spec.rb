2cdb37745f790ac9542a54251d648fd6762385f6
Fred Rondina <fredrondina96@gmail.com>
2020-04-12 19:02:32 -0600

    added passing test for US 35, and additional item for testing orders

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'As an merchant user', type: :feature do
      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
      @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+     @bottle = @meg.items.create(name: "WaterBottle", description: "its a water battole", price: 20, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 24)
      @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
      @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
      @order_2 = @user.orders.create!(name: 'Brian', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: "packaged")
@@ -15,6 +16,7 @@ RSpec.describe 'As an merchant user', type: :feature do
      @order_4 = @user2.orders.create!(name: 'Bryan', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: "canceled")
      @item_order_tire = @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
      @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3)
+     @order_1.item_orders.create!(item: @bottle, price: @bottle.price, quantity: 3)
      @order_2.item_orders.create!(item: @tire, price: @tire.price, quantity: 7)
      @order_3.item_orders.create!(item: @tire, price: @tire.price, quantity: 9)
      @order_4.item_orders.create!(item: @tire, price: @tire.price, quantity: 9)
@@ -31,6 +33,15 @@ RSpec.describe 'As an merchant user', type: :feature do
       expect(page).to have_content(@meg.city)
       expect(page).to have_content(@meg.state)
       expect(page).to have_content(@meg.zip)
+    end
+
+    it "I see all pending orders containing items I sell" do
+      visit merchant_dashboard_path
+
+      expect(page).to have_link(@order_1.id)
+      expect(page).to have_content(@order_1.created_at)
+      expect(page).to have_content("My Items: 5")
+      expect(page).to have_content("Value of My Items: $260")
     end
   end
   after(:each) do
