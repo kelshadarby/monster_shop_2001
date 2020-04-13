cf07bb395c90489be85eb90e2bdd0e9bd6f32c88
Fred Rondina <fredrondina96@gmail.com>
2020-04-12 19:01:40 -0600

    added 2 helper methods, total merchant items per order, & total cost

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,12 +12,19 @@ class Order <ApplicationRecord
   def total_items
     item_orders.sum('quantity')
   end
-  
+
   def cancel
     item_orders.each do |item_order|
       item_order.unfulfill
     end
     self.update(status: "canceled")
   end
-  
+
+  def number_of_items_for_merchant(merchant_id)
+    item_orders.joins(:item).where(items: {merchant_id: merchant_id}).sum(:quantity)
+  end
+
+  def total_cost_for_merchant(merchant_id)
+    item_orders.joins(:item).where(items: {merchant_id: merchant_id}).sum('item_orders.quantity * item_orders.price')
+  end
 end
