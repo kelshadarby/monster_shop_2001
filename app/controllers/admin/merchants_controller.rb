f5cf0b87f5da5a772af0e64240dd9e3ed8081451
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-13 15:17:08 -0600

    Add to admin merchants controller to deactivate items when merchant is disabled and vise versa

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,9 +7,11 @@ class Admin::MerchantsController < Admin::BaseController
     merchant = Merchant.find(params[:merchant_id])
     if merchant.active?
       merchant.disable
+      merchant.items.each { |item| item.deactivate }
       flash[:disabled] = "#{merchant.name} has been disabled."
     else
       merchant.enable
+      merchant.items.each { |item| item.activate }
       flash[:enabled] = "#{merchant.name} has been enabled."
     end
     redirect_to "/admin/merchants"
