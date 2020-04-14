306977c63064347b67f61f5fc9b8f3bdeefeb52e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-14 12:49:25 -0600

    Add merchant order show page test

---
+++ spec/features/merchant/orders/show_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'As an merchant user', type: :feature do
+  before(:each) do
+    @user = User.create!( email_address: 'user1@example.com', password: 'password', role: 'default', name: 'User 1', street_address: '123 Example St', city: 'Userville', state: 'State 1', zip_code: '12345')
+    @user2 = User.create!( email_address: 'user2@example.com', password: 'password', role: 'default', name: 'User 2', street_address: '123 Example St', city: 'Userville', state: 'State 1', zip_code: '12345')
+    @merchant1 = User.create!( email_address: 'merchant@example.com', password: 'password', role: 2, name: 'merchant', street_address: '123 admin St', city: 'adminville', state: 'State 5', zip_code: '54321')
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @bottle = @meg.items.create(name: "WaterBottle", description: "its a water battole", price: 20, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 24)
+    @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+    @order_2 = @user.orders.create!(name: 'Brian', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: "packaged")
+    
+    @order_1_pull_toy = @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3)
+    @order_1_bottle = @order_1.item_orders.create!(item: @bottle, price: @bottle.price, quantity: 4)
+    @order_2_tire = @order_2.item_orders.create!(item: @tire, price: @tire.price, quantity: 7)
+
+
+    @meg.users << @merchant1
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant1)
+  end
+
+  describe 'On my order show page' do
+    it 'I can see the details of an order' do
+      visit merchant_order_show_path(@order_1)
+save_and_open_page
+      expect(page).to have_content(@order_1.name)
+      expect(page).to have_content(@order_1.address)
+      expect(page).to have_content(@order_1.state)
+      expect(page).to have_content(@order_1.zip)
+      expect(page).to have_link(@bottle.name, href: item_show_path(@bottle))
+      expect(page).to have_css("img[src*='#{@bottle.image}']")
+      expect(page).to have_content(@order_1_bottle.price)
+      expect(page).to have_content(@order_1_bottle.quantity)
+      
+      expect(page).to_not have_link(@pull_toy.name, href: item_show_path(@pull_toy))
+      expect(page).to_not have_link(@tire.name, href: item_show_path(@tire))
+    end
+  end
+
+  after(:each) do
+    ItemOrder.destroy_all
+    Order.destroy_all
+    User.destroy_all
+    Merchant.destroy_all
+  end
+end
