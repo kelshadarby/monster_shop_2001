72cb54baf31648d18590d2b1ecd596e2f24d8815
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-14 10:37:57 -0600

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ItemOrder.destroy_all
+Order.destroy_all
+User.destroy_all
 Merchant.destroy_all
-Item.destroy_all
 
 #merchants
 bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
