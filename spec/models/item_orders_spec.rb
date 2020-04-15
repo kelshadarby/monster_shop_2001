c171be38e6ccf9153f53064eeb458f879395c45c
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-15 16:47:22 -0600

    Expand item order fulfill test

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -18,8 +18,10 @@ describe ItemOrder, type: :model do
       @user = User.create!(email_address: 'user1@example.com', password: 'password', role: 'default', name: 'User 1', street_address: '123 Example St', city: 'Userville', state: 'State 1', zip_code: '12345')
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @tire_2 = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
       @item_order_1 = @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      @item_order_2 = @order_1.item_orders.create!(item: @tire_2, price: @tire_2.price, quantity: 3)
       
     end
     it 'subtotal' do
@@ -27,10 +29,18 @@ describe ItemOrder, type: :model do
     end
 
     it 'fulfill' do
+      expect(@order_1.status).to eq("pending")
+
       @item_order_1.fulfill
-      
+      @order_1.reload
+      expect(@order_1.status).to eq("pending")
       expect(@item_order_1.status).to eq("fulfilled")
       expect(@tire.inventory).to eq(10)
+
+      @item_order_2.fulfill
+      @order_1.reload
+
+      expect(@order_1.status).to eq("packaged")
     end
 
     it 'fulfilled?' do
